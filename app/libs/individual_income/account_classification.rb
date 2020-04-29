cdf108c2806bb98562bab8ca0395a471792a0b99
steamshon <steamshon@apache.org>
2020-04-29 11:57:19 +0900

    tmp

---
+++ app/libs/individual_income/account_classification.rb
@@ -0,0 +1,12 @@
+module IndividualIncome
+  class AccountClassification
+    def self.load
+
+    end
+
+    def self.classify(declarator_category: ,vendor_hometax_classification_code:)
+      code = vendor_hometax_classification_code[0..1]
+
+    end
+  end
+end
