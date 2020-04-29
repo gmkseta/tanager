fef83fb5d54974b636822a1bc3c42912e3f55c0b
steamshon <steamshon@apache.org>
2020-04-29 16:10:56 +0900

    add seed data on db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def parse_to_hash(file_path, col_sep = ",", columns = nil)
+  rows = CSV.read(file_path, col_sep: col_sep)
+  header = rows[0]
+  selected_columns = columns.nil? ? header : columns
+
+  rows[1..-1].map do |row|
+    header.zip(row).to_h.select { |k, v| selected_columns.include?(k.to_s) }
+  end
+end
+
+parse_to_hash("#{Dir.getwd}/classification_code_categories.csv").map do |params|
+  ClassificationCodeCategory.create!(params)
+end
+
+parse_to_hash("#{Dir.getwd}/account_classification_rules.csv").map do |params|
+  AccountClassificationRule.create!(params)
+end
