49a743ad186f50bb7c95ddcc4590ddf25a793c8d
steamshon <steamshon@apache.org>
2020-04-30 10:00:24 +0900

    add upsert on simplified_bookkeeping

--- app/models/simplified_bookkeeping.rb
+++ app/models/simplified_bookkeeping.rb
@@ -1,2 +1,14 @@
 class SimplifiedBookkeeping < ApplicationRecord
+
+  class << self
+    def upsert(rows:)
+      SimplifiedBookkeeping.import!(
+        rows,
+        on_duplicate_key_update: {
+            conflict_target: %i(registration_number vendor_registration_number purchase_type),
+            columns: %i(account_classification_code account_classification_name amount purchases_count updated_at),
+        },
+      )
+    end
+  end
 end
