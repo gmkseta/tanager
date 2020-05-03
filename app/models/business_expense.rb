6a7b5b7048e6883f7aa098dbb7f37ec0260f4571
beck lee <tigerjj84@gmail.com>
2020-05-04 03:24:47 +0900

    Add validation for classification

--- app/models/business_expense.rb
+++ app/models/business_expense.rb
@@ -3,6 +3,7 @@ class BusinessExpense < ApplicationRecord
   belongs_to :expense_classification, class_name: "Classification", foreign_key: :expense_classification_id
   belongs_to :account_classification, class_name: "Classification", foreign_key: :account_classification_id, optional: true
 
-  validates :expense_classification_id, presence: true
+  validates :expense_classification_id, presence: true, inclusion: { in: Classification.business_expenses, message: :invalid_type }
+  validates :account_classification_id, inclusion: { in: Classification.account_classifications, message: :invalid_type }
   validates :amount, presence: true, numericality: { greater_than: 0, message: :greater_than_zero }
 end
