1d6831c9e2093218d229ddaed40b7be926057358
beck lee <tigerjj84@gmail.com>
2020-05-04 21:48:26 +0900

    fixup! Change nullable to hometax_individual_incomes

--- app/models/business_expense.rb
+++ app/models/business_expense.rb
@@ -3,7 +3,7 @@ class BusinessExpense < ApplicationRecord
   belongs_to :expense_classification, class_name: "Classification", foreign_key: :expense_classification_id
   belongs_to :account_classification, class_name: "Classification", foreign_key: :account_classification_id, optional: true
 
-  validate :expense_classification_id, :validate_unique_expense?
+  validate :expense_classification_id, :validate_unique_expense?, on: :create
   validates :expense_classification_id, presence: true, inclusion: { in: Classification.business_expenses.ids, message: :invalid_type }
   validates :account_classification_id, inclusion: { in: Classification.account_classifications.ids, message: :invalid_type }, allow_nil: true
   validates :amount, presence: true, numericality: { greater_than: 0, message: :greater_than_zero }
