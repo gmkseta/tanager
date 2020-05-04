4708f1d333899f48f1903232feea1861f880e19c
beck lee <tigerjj84@gmail.com>
2020-05-04 19:29:24 +0900

    Allow nil to business expense

--- app/models/business_expense.rb
+++ app/models/business_expense.rb
@@ -4,6 +4,6 @@ class BusinessExpense < ApplicationRecord
   belongs_to :account_classification, class_name: "Classification", foreign_key: :account_classification_id, optional: true
 
   validates :expense_classification_id, presence: true, inclusion: { in: Classification.business_expenses.ids, message: :invalid_type }
-  validates :account_classification_id, inclusion: { in: Classification.account_classifications.ids, message: :invalid_type }
+  validates :account_classification_id, inclusion: { in: Classification.account_classifications.ids, message: :invalid_type }, allow_nil: true
   validates :amount, presence: true, numericality: { greater_than: 0, message: :greater_than_zero }
 end
