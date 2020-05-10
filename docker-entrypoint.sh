4a87ba805055bd79c39caf09c15a2368018194b5
beck lee <tigerjj84@gmail.com>
2020-05-10 15:19:01 +0900

    Temporary run bundle update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-bundle exec rails db:create db:migrate
+bundle exec db:migrate
+bundle exec bundle update
 
 bundle exec puma -C config/docker_puma.rb -p 3000
 
