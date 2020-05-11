cbfd93269fb14036171523ae4c1334c7b5aee87b
beck lee <tigerjj84@gmail.com>
2020-05-12 03:22:01 +0900

    fixup! Add social_insurance sum amount in business expenses

--- db/migrate/20200511141858_add_nullable_to_owner_id_in_hometax_individual.rb
+++ db/migrate/20200511141858_add_nullable_to_owner_id_in_hometax_individual.rb
@@ -7,7 +7,6 @@ class AddNullableToOwnerIdInHometaxIndividual < ActiveRecord::Migration[5.2]
     change_column_null :hometax_individual_incomes, :owner_id, false
     change_column_null :hometax_individual_incomes, :declare_year, false
 
-    add_index :hometax_individual_incomes, [:owner_id, :declare_year],
-      name: "index_hometax_individual_incomes_on_declare_user_id_and_year", unique: true, algorithm: :concurrently
+    add_index :hometax_individual_incomes, [:owner_id, :declare_year], unique: true, algorithm: :concurrently
   end
 end
