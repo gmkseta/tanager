4ad8d1aa06c3c590b9f328769d9312e056cacb3e
beck lee <tigerjj84@gmail.com>
2020-05-12 05:47:04 +0900

    fixup! Add hometax_address phone in user, taxation_month delcare_date in declare_user

--- db/migrate/20200511193130_add_unique_index_to_declare_user.rb
+++ db/migrate/20200511193130_add_unique_index_to_declare_user.rb
@@ -2,6 +2,5 @@ class AddUniqueIndexToDeclareUser < ActiveRecord::Migration[5.2]
   def change
     add_index :declare_users, [:user_id, :declare_tax_type, :taxation_month],
       name: "index_decalre_users_on_user_and_tax_type_and_taxation_month", unique: true
-    add_foreign_key :declare_users, :users, column: :user_id, on_delete: :cascade
   end
 end
