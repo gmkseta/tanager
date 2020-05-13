3700ea9e564ee9090e598acedd469643b93cbc59
beck lee <tigerjj84@gmail.com>
2020-05-13 11:28:54 +0900

    Bug fix to allow expense by personal card

--- app/models/business_expense.rb
+++ app/models/business_expense.rb
@@ -4,7 +4,7 @@ class BusinessExpense < ApplicationRecord
   belongs_to :account_classification, class_name: "Classification", foreign_key: :account_classification_id, optional: true
 
   validate :expense_classification_id, :validate_unique_expense?, on: :create
-  validates :expense_classification_id, presence: true, inclusion: { in: Classification.business_expenses.ids, message: :invalid_type }
+  validates :expense_classification_id, presence: true, inclusion: { in: Classification.business_expenses.ids + Classification.personal_cards.ids, message: :invalid_type }
   validates :account_classification_id, inclusion: { in: Classification.account_classifications.ids, message: :invalid_type }, allow_nil: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0, message: :greater_than_or_equal_to_zero }
   validates :vendor_registration_number, format: { with: /\A\d{10}\z/ }, allow_nil: true
