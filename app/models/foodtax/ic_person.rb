6fbe857a3df536786193af879e69f82b3111e2ac
beck lee <tigerjj84@gmail.com>
2020-05-15 21:06:00 +0900

    Add sample to initialize ic_person by declare_user

--- app/models/foodtax/ic_person.rb
+++ app/models/foodtax/ic_person.rb
@@ -1,16 +1,20 @@
 module Foodtax
   class IcPerson < Foodtax::ApplicationRecord
     include FoodtaxHelper
-    self.primary_keys = :member_cd, :cmpy_cd, :term_cd, :declare_seq
-    self.table_name = "VA_ELEC_FILE_CONTENTS"
+    self.primary_keys = :cmpy_cd, :person_cd
     after_initialize :default_user_id
 
-    belongs_to :cm_member, foreign_key: :member_cd, primary_key: :member_cd
-    belongs_to :va_head, foreign_key: :member_cd, primary_key: :member_cd
-
-    private
-
-    def default_user_id
+    def initialize_by_declare_user(declare_user, member_cd)
+      self.cmpy_cd = "00025"
+      self.person_cd = "P#{"%06d" % declare_user.id}"
+      self.jumin_no = declare_user.residence_number
+      self.name = declare_user.name
+      self.member_cd = member_cd
+      address = declare_user.hometax_address || declare_user.address
+      split_address = address.split
+      self.addr1 = split_address[0]
+      self.addr2 = split_address[1]
+      self.tel_no = declare_user.phone_number
     end
   end
 end
