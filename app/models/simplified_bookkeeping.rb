2899125ce64bb600ac94db020720adc6b445cfdc
beck lee <tigerjj84@gmail.com>
2020-05-17 00:01:39 +0900

    Add deductible from HometaxCardPurchases and misssing deductible from UserRule

--- app/models/simplified_bookkeeping.rb
+++ app/models/simplified_bookkeeping.rb
@@ -15,7 +15,7 @@ class SimplifiedBookkeeping < ApplicationRecord
         on_duplicate_key_update: {
             conflict_target: %i(registration_number vendor_registration_number purchase_type),
             index_predicate: "vendor_registration_number IS NOT NULL",
-            columns: %i(account_classification_code classification_id amount purchases_count updated_at),
+            columns: %i(account_classification_code classification_id amount purchases_count updated_at deductible),
         },
       )
     end
