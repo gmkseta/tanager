a5acd15ec26ea6bf627e26a56da0a155632c18cd
beck lee <tigerjj84@gmail.com>
2020-05-24 03:51:36 +0900

    Enable sidekiq jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,18 @@
-redis_options = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/1" }
+if ENV["REDIS_HOST"]
+  redis_options = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/1" }
+else
+  redis_options = { url: "redis://127.0.0.1:6379/1" }
+end
 
 if Rails.env.test?
   redis_options[:driver] = Redis::Connection::Memory if defined?(Redis::Connection::Memory)
 end
 
 Sidekiq.configure_server do |config|
+  config.redis = redis_options
+end
+
+Sidekiq.configure_client do |config|
   config.redis = redis_options
 end
 Sidekiq::Extensions.enable_delay!
